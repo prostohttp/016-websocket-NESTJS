2c1c5c09aeb3e7e69441eb50553e33d0bcf25eb2
Morozov Mikhail <memphis.sail@gmail.com>
2023-11-22 23:53:13 +1000

    v.1.0.1

--- src/gateway/app.gateway.ts
+++ src/gateway/app.gateway.ts
@@ -4,6 +4,7 @@ import {
   WebSocketGateway,
 } from "@nestjs/websockets";
 import { BookCommentsService } from "src/comments/book.comments.service";
+import { CreateCommentDto } from "src/comments/dto/create.comment.dto";
 
 @WebSocketGateway({ cors: true })
 export class AppGateway {
@@ -15,7 +16,7 @@ export class AppGateway {
   }
 
   @SubscribeMessage("add-comment")
-  addComment(@MessageBody() comment: any) {
+  addComment(@MessageBody() comment: CreateCommentDto) {
     return this.bookCommentsService.create(comment);
   }
 }
